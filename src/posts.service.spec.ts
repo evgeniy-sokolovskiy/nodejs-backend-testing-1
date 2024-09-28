a831f46b5459f28d5df1fe484d756804dfe91d5a
evgeniy <sokol-rc@yandex.ru>
2024-09-28 13:14:06 +0300

    add post service unit-tests

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -5,18 +5,27 @@ describe('PostsService', () => {
   const post: Omit<Post, 'id' | 'date'> = {
     text: 'Mocked post',
   };
+  let createdPost: Post
+  const DEFAULT_NEW_POST = { text: 'Some pre-existing post' }
 
   beforeEach(async () => {
     postsService = new PostsService();
-
-    postsService.create({ text: 'Some pre-existing post' });
+    createdPost = postsService.create(DEFAULT_NEW_POST);
   });
 
   it('should add a new post', () => {
     // реализуйте тест-кейс
+    expect(createdPost).toStrictEqual({
+      ...DEFAULT_NEW_POST,
+      id: createdPost.id,
+      date: createdPost.date,
+    });
   });
 
   it('should find a post', () => {
     // реализуйте тест-кейс
+    const NEW_POST = {text: 'Some pre-existing post'}
+    const { id } = postsService.create(NEW_POST);
+    expect(postsService.find(id)).toEqual(expect.objectContaining(NEW_POST));
   });
 });
